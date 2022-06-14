750c2e2d707a71284e1234b1521c04efa12be981
GautamChopra467 <77351283+GautamChopra467@users.noreply.github.com>
2022-06-14 01:39:45 -0700

    Update Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,9 @@
 import React from "react";
 import "./HomeStyles.css";
 import { Link } from "react-router-dom";
-import Logo from "../assets/main.png";
-
-import Form from "./Form";
 import Navbar from "./Navbar";
-// rgba(191,219,254,var(--tw-bg-opacity));
+
 const Home = () => {
-  // const [counter, setCounter] = useState(false);
   return (
     <div className="mainContainer">
       <Navbar />
@@ -15,7 +11,6 @@ const Home = () => {
         <h1>Making healthcare accessible using</h1>
         <h2>AI powered solutions</h2>
         <Link to={'/form'} className='button'>Prescription Form</Link>
-        {/* <button>Prescription Form</button> */}
       </div>
     </div>
   );
